01d3493f40ea1628907b044f3e716da1fc2bcf25
Jason <Jason@JASON-i5577KDG5>
2020-10-27 01:15:48 -0400

    CPTR421 Project 1

---
+++ src/AreaOfSquare.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public abstract class AreaOfSquare {
+   public static void main (String[] args)
+   {
+	   Scanner scanner = new Scanner(System.in);
+	   System.out.println("Please enter the length of the rectangle:");
+	   double length = scanner.nextDouble();
+	   System.out.println("Please enter the width of rectangle:");
+	   double width = scanner.nextDouble();
+	   //length*width;
+	   double area = length*width;
+	   System.out.println("The area of rectangle is:"+area);
+   }
+}
