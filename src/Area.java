94caefeba6428843e08685f57b490c34f4e50e84
Jason <Jason@JASON-i5577KDG5>
2020-10-28 23:59:44 -0400

    CPTR421 Project 1

--- src/Area.java
+++ src/Area.java
@@ -1,6 +1,30 @@
-import java.util.Scanner;
-
-
-public abstract class Area {
+public class Area{
     
+    public newShape areaShape(String getOption){
+        
+      
+        
+        if(getOption.equals("Triangle")){
+            
+            return new AreaOfTriangle(); 
+        } else
+            
+            if(getOption.equals("Rectangle")){
+                
+                return new AreaOfRectangle();
+            
+            } else
+                
+                if(getOption.equals(Square)){
+                    
+                    return new AreaOfSquare();
+                    
+                } else return null;
+    }
+
+    private static class newShape {
+
+        public newShape() {
+        }
+    }
 }
